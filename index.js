4a6cc7113fa82c5b1a96329f674e9fc8139a12fb
Jayden Ly <jaydenly.ca@gmail.com>
2026-06-23 14:03:19 -0400

    fix: use clientReady instead of client since its deprecated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
   ],
 });
 
-client.once('ready', async () => {
+client.once('clientReady', async () => {
   console.log(` Logged in as ${client.user.tag}`);
   console.log(` Registering slash commands...`);
   
